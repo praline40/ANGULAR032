d2ef0eae8d373c63df153ef5191f80dee7e51e12
Jules <praline40@hotmail.fr>
2020-05-30 08:55:45 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'challenge32';
+export class AppComponent implements OnInit{
+  ngOnInit(){
+  }
+  title = "Bonjour !";
+  isAdmin: boolean = false;
+  showGuests: boolean = false; 
+  showFriends: boolean = false; 
+  isThisGuestImportant: boolean=true;
+  showFavoriteMovies: boolean = true; 
+  showHorrorMovies: boolean = true; 
+
+  //Fonction appelée lors du clic
+  btnShowFavoriteMovies_OnClick(){
+    this.showFavoriteMovies= !this.showFavoriteMovies;
+  }
+
+  //Fonction appelée lors du clic
+  btnShowHorrorMovies_OnClick(){
+    this.showHorrorMovies= !this.showHorrorMovies;
+  }
+
+  btnShowGuests_OnClick(){
+    this.showGuests= !this.showGuests;
+  }
+
+  btnShowFriends_OnClick(){
+    this.showFriends= !this.showFriends;
+  }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HighlightDirective } from './highlight.directive';
+import { DisplayDirective } from './display.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HighlightDirective, 
+    DisplayDirective
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
